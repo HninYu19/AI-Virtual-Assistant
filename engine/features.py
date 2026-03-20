3fa1f26485e1b52fe4254b9b0770d57aea3f5485
Hnin Yu <hninyu12915@gmail.com>
2026-03-20 14:15:59 +0800

    Voice Command

--- engine/features.py
+++ engine/features.py
@@ -1,5 +1,8 @@
 from playsound import playsound
+import eel
 
+#play sound 
+@eel.expose
 def play_sound():
     music_dir = "website/assets/texllate/audio/start_sound.mp3"
     playsound(music_dir)
